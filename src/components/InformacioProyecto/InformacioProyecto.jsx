5e72a2821d048fe4b097b692f9427a8d893bc3c6
Jose <josemania10@gmail.com>
2021-02-23 00:04:17 -0500

    Cambios en la estructura del componente

--- src/components/InformacioProyecto/InformacioProyecto.jsx
+++ src/components/InformacioProyecto/InformacioProyecto.jsx
@@ -1,28 +1,36 @@
 import React from 'react';
 import './styles.js'
-import {Wrapper,Titulo, Emprendedor} from './styles'
-
+import { Titulo, Emprendedor} from './styles'
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { Avatar } from '@material-ui/core';
 
 export const InformacioProyecto = ({
                                     fotoPerfil,
                                     nombreProyecto,
                                     nombreUsuario,
-}
+}) => {
 
-) => (
-  <div>
-    <Wrapper>
-      <img src={fotoPerfil} width="60" height="60" ></img>
-     
-        <Titulo>
-          <text >{nombreProyecto}</text>
-        </Titulo>
-        <br/>
-        <Emprendedor>
-          <text >{nombreUsuario}</text>
-        </Emprendedor>
-    </Wrapper>
-    
-  </div>
-);
+  return(
+    <div>
+      <Grid container spacing={0} justify="center" alignItems="flex-start">
+        <Grid item xs={1}>
+          <Avatar variant = 'square'src={fotoPerfil}/> 
+        </Grid>
+        <Grid item xs={8}>
+            <Titulo>
+              <text >{nombreProyecto}</text>
+            </Titulo>
+        </Grid>
+        <Grid item xs={1}/>
+        <Grid item xs={8}>
+          <Emprendedor>
+            <text >{nombreUsuario}</text>
+          </Emprendedor>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
 
