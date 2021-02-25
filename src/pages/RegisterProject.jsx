dfc2471b043b44d8ee5274aeeb08c2de75cc4eaf
Alejandro Bohórquez <53972708+AlejandroBohal@users.noreply.github.com>
2021-02-24 20:59:44 -0500

    ProjectFinancing component (#8)
    * Progress bar added
    
    * Removed code smells
    
    * Solved jsx
    
    * Finished Ui, missing useMemo for projectRegister form
    
    * Fixed code smells
    
    * Fixed useMemo
    
    * Finished projectFinancing component
    
    * Finisished Project Financing
    
    * ProjectPresentation
    
    * Solved CodeSmells

--- src/pages/RegisterProject.jsx
+++ src/pages/RegisterProject.jsx
@@ -1,9 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState,useCallback } from 'react'
 import { ProgressBar } from '../components/Progressbar/ProgressBar'
-import { makeStyles, Typography } from '@material-ui/core';
+import { makeStyles, Fab } from '@material-ui/core';
 import steps from './steps';
 import {ProjectDescription} from '../components/ProjectForm/ProjectDescription';
+import {ProjectFinancing} from '../components/ProjectForm/ProjectFinancing';
+import {ProjectPresentation} from '../components/ProjectForm/ProjectPresentation';
 import {useForm} from "../hooks/useForm";
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import {ThemeProvider} from "@material-ui/core/styles";
+import globalTheme from "../globalTheme";
+
 const useStyles = makeStyles({
     root: {
         '@media (min-width:480px)': { 
@@ -18,59 +24,67 @@ const useStyles = makeStyles({
         '@media (min-width:1025px)': { 
             width: '50%'
         },
-        margin: "6rem auto",
+        margin: "4rem auto",
     }    
 });
 
 export const RegisterProject = () => {
     const [activeStep, setActiveStep] = useState(0);
-    const prevMessage = activeStep === steps.length ? "" : steps[activeStep-1];
-    const handleNext = () => {
-        setActiveStep(prevActiveStep => prevActiveStep + 1);
-    }
+    const handleNext = useCallback(() => {
+        setActiveStep(prevActiveStep => prevActiveStep + 1)}
+    );
     const handlePrev = () => {
         setActiveStep(prevActiveStep => prevActiveStep -1)
     }
     const [inputValues,handleInputChange] = useForm({
         projectName: '',
-        projectDescription: ''
+        projectDescription: '',
+        investment: 0,
+        minInvestment: 0,
     });
+    const [dueDate,setDueDate] = useState(new Date());
+    const handleDateChange = useCallback((date) => setDueDate(date));
+    const handleChange = useCallback(handleInputChange);
     const classes = useStyles();
+    const dueDateString = dueDate.toDateString();
     const getStepContent = (stepIndex) => {
         switch (stepIndex) {
             case 0:
                 return <ProjectDescription 
                         formValues={inputValues}
-                        handleChange={handleInputChange}
+                        handleChange={handleChange}
                         handleNext={handleNext}/>;
             case 1:
-                return "Paso 2 Financiación del proyecto";
+                return <ProjectFinancing 
+                        formValues={inputValues}
+                        handleChange={handleChange}
+                        handleNext={handleNext}
+                        zone={{zone:"co-CO",region:"COP"}}
+                        handleDateChange = {handleDateChange}
+                        />;
             case 2:
-                return "Paso 3 Presentación del proyecto";
+                return <ProjectPresentation/>;
             default:
-                return "Nada para hacer";
+                return "Nada para hacer" + dueDateString;
         }
     }
     return (
-        <div className={classes.root}>
-            <ProgressBar activeStep={activeStep}/>
-            {activeStep === steps.length ? "Finished": (
+        <ThemeProvider theme={globalTheme}>
+            <div className={classes.root}>
+                <ProgressBar activeStep={activeStep}/>
+                {activeStep === steps.length ? "Finished": (
+                        <>
+                            {getStepContent(activeStep)}
+                        </>
+                )}
+                {activeStep !== 0 && activeStep < steps.length &&(
                     <>
-                        {getStepContent(activeStep)}
+                        <Fab size="small" color="primary" aria-label="edit" onClick={handlePrev}>
+                            <ArrowBackIcon />
+                        </Fab>        
                     </>
-            )}
-            {activeStep !== 0 && activeStep < steps.length &&(
-                <>
-                    <Typography 
-                        variant="body1" 
-                        color="initial" 
-                        style={{cursor:"pointer"}} 
-                        onClick={handlePrev}>
-                        {prevMessage}
-                    </Typography>      
-                    
-                </>
-            )}
-        </div>
+                )}
+            </div>
+        </ThemeProvider>
     )
 }
