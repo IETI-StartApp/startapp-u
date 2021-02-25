f2d031860ef3690ca6a7cb2e12a4dec5ba92f56c
José Luis Gómez C <46968912+Jose-Gomez-C@users.noreply.github.com>
2021-02-24 20:32:10 -0500

    Story/videocomponent (#10)
    * Finalización de la historia Project component
    
    * Corrección de errores
    
    * Cambios en la estructura del componente
    
    * Combios sugeridos por sonarcloud
    
    * Corrección de errores.
    
    * Componente video terminado

---
+++ src/components/InformacioProyecto/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Titulo = styled.text`
+font-family: Robot;
+font-style: Robot;
+font-weight: Robot;
+font-size: 30px;
+line-height: 30px;
+letter-spacing: 1.1px;
+`;
+export const Emprendedor = styled.text`
+font-family: Robot;
+font-style: Robot;
+font-weight: Robot;
+font-size: 15px;
+line-height: 27px;
+`;
