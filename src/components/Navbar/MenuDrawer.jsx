d7cc52bc640fa4c2ffe1ca5dc04bfe2893cca074
angipaola10 <angi.jimenez@mail.escuelaing.edu.co>
2021-02-25 15:46:50 -0500

    Drawer

---
+++ src/components/Navbar/MenuDrawer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Drawer } from '@material-ui/core/';
+
+export default function MenuDrawer( ){
+
+    return (
+        <Drawer 
+            anchor="left"
+            variant="temporary"
+        >
+        </Drawer>
+    );
+}
