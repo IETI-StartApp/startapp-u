14f3e849b54715de2bb12ef5f8520a367e07d8d5
FelipeRojas15 <53972640+FelipeRojas15@users.noreply.github.com>
2021-02-24 21:54:02 -0500

    Feat(Register-component) (#9)
    * Feat(Register-component)
    
    * Fixed code smells
    
    * Fixed Security Hotspots
    
    * Fixed Hotspots

---
+++ src/components/Register/RegisterForm.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Register from './Register'
+
+
+
+const RegisterForm = () => {
+    return (
+        
+        <div>
+            <Register/>
+        </div>
+            
+    )
+}
+
+export default RegisterForm
